e8c1adb49e40ec2d7d01761dcf7a7edf13374a93
Hoang Xuan Viet <20522149@gm.uit.edu.vn>
2024-01-19 21:06:07 +0700

    Adding Routing + Fragment and Ping with ICMPv6

--- udp_scapy.py
+++ udp_scapy.py
@@ -9,19 +9,25 @@ def select_and_send(i, addr, dport, sport):
         pkt = IPv6(dst=addr)/UDP(dport=dport, sport=sport)/b"CE_UIT"
     if i == 2:
         print("List of optional Next Header: ")
-        print("0. Hop-By-Hop OPtions.")
+        print("0. Hop-By-Hop Options.")
         print("43. Routing.")
         print("44. Fragment.")
         print("60. Destination Options.")
+        print("1. Routing + Fragment.")
+        print("2. Ping.")
         next_header = int(input("Insert your Next Header option: "))
         if next_header == 0:
             pkt = IPv6(dst=addr)/IPv6ExtHdrHopByHop(options=Jumbo(jumboplen=2**30))/UDP(dport=dport, sport=sport)/b"HopByHop"
         if next_header == 43:
             pkt = IPv6(dst=addr)/IPv6ExtHdrRouting(type=0, segleft=0)/UDP(dport=dport, sport=sport)/b"Routing"
         if next_header == 44:
-            pkt = IPv6(dst=addr)/IPv6ExtHdrFragment(m=1)/UDP(dport=dport, sport=sport)/b"Fragment"
+            pkt = IPv6(dst=addr)/IPv6ExtHdrFragment(m=1, id=1)/UDP(dport=dport, sport=sport)/b"Fragment"
         if next_header == 60:
             pkt = IPv6(dst=addr)/IPv6ExtHdrDestOpt()/UDP(dport=dport, sport=sport)/b"Destinantion Options"
+        if next_header == 1:
+            pkt = IPv6(dst=addr)/IPv6ExtHdrRouting(type=0, segleft=0)/IPv6ExtHdrFragment(m=0, id=1)/UDP(dport=dport, sport=sport)/b"Routing + Fragment"
+        if next_header == 2:
+            pkt = IPv6(dst=addr)/ICMPv6EchoRequest(data="Ping")
     # s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
     # s.bind(('', sport))
     # s.sendto(pkt, (ip.dst, dport))
